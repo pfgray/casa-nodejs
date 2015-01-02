35d58cd0dfc73e68a475f8ce34f24e7a12631e0a
Paul Gray <pfbgray@gmail.com>
2015-01-02 17:52:22 -0500

    Now returning app with most recent timestamp

--- server/api/application/application.model.js
+++ server/api/application/application.model.js
@@ -14,11 +14,30 @@ module.exports = {
     },
     getApplication:function(originatorId, appId, callback){
         var db = casa_model.getDatabase();
-        db.view('casa/apps_by_id', {key:{originator_id: originatorId, id: appId}}, function (err, res) {
-            //TODO: is there a better way to find a single entity?
-            callback(err, _.transform(res, function(result, entity){
-                return result.push(entity.value);
-            })[0]);
+        db.view('casa/apps_with_timestamp', {
+            startkey:[{originator_id: originatorId, id: appId}],
+            endkey:[{originator_id: originatorId, id: appId}, {}]
+        }, function (err, res) {
+            console.log('got apps: ', JSON.stringify(res));
+            //iterate through these to find the one with the most recent timestamp
+            if(res.length > 0){
+                var mostRecentTimestamp = 0;
+                var mostRecentAppIndex = 0; //default to the first app found
+                _.each(res, function(res, index){
+                    var currentAppTimestamp = new Date(res.key[1]);
+                    console.log('got app with timestamp: ', currentAppTimestamp);
+                    console.log('    converted to milliseconds: ', currentAppTimestamp.getTime());
+                    console.log('    checking against: ', currentAppTimestamp);
+                    if(currentAppTimestamp.getTime() > mostRecentTimestamp){
+                        mostRecentAppIndex = index;
+                        mostRecentTimestamp = currentAppTimestamp.getTime();
+                    }
+                });
+                console.log('returning app with timestamp: ', res[mostRecentAppIndex].key[1])
+                callback(null, res[mostRecentAppIndex].value);
+            } else {
+                callback(null, null);
+            }
         });
     }
 }
--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // CouchDB connection options
   couch: {
-    host: process.env.COUCH_HOST || 'http://nexus',
+    host: process.env.COUCH_HOST || 'http://localhost',
     port: process.env.COUCH_PORT || 5984,
     db_name: process.env.COUCH_DB_NAME || 'casa'
   }
