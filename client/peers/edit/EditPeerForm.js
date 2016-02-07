0de646cefd17daa5b4059ca0920924a07cf41ff3
Paul Gray <pfbgray@gmail.com>
2016-02-07 18:19:59 -0500

    Adding styles for forms

--- client/peers/edit/EditPeerForm.js
+++ client/peers/edit/EditPeerForm.js
@@ -6,10 +6,10 @@ export default (props) => {
     <div className='content'>
       <div className='panel peer-edit'>
         <form onSubmit={handleSubmit(createPeer)}>
-          <input type='text' name='name' placeholder='name' {...name} />
+          <input type='text' name='name' placeholder='Name' {...name} />
           <input type='text' name='payloadUrl' placeholder='Payload Url' {...payloadUrl}/>
-          <button type="submit" disabled={submitting}>
-            {submitting ? <i/> : <i/>} Submit
+          <button className='btn' type="submit" disabled={submitting}>
+            {submitting ? <i className='fa fa-circle-o-notch fa-spin'/> : null} Submit
           </button>
         </form>
       </div>
