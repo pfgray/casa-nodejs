53eafaadfb3491772f01dca9ab9b6496df5daf55
Paul Gray <pfbgray@gmail.com>
2016-02-29 22:43:44 -0500

    Adding styles to intro page

--- client/welcome/Welcome.js
+++ client/welcome/Welcome.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class Welcome extends React.Component {
   render(){
     return (
-      <span>hmmasdf!</span>
+      <div className="welcome-page">
+        <h1>CASA</h1>
+        <div className='subtitle'>
+          Create a community of applications!
+        </div>
+        <Link className='btn link-btn clear-btn' to='/login'>
+          Login / Signup
+        </Link>
+      </div>
     );
   }
 }
