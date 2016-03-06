23cbd0ab9db4276b5a7aaaf431600ec8a88919dc
Paul Gray <pgray@learningobjects.com>
2016-03-06 12:01:05 -0500

    Refactoring gradient barckground for better animations

--- client/CasaApp.js
+++ client/CasaApp.js
@@ -5,26 +5,31 @@ import React from 'react';
 import './styles/main.less';
 import HeaderContainer from './header/HeaderContainer';
 import ReactCSSTransitionReplace from 'react-css-transition-replace';
+import classNames from 'classnames';
 
-const reouteTransitionDuration = 1000;
+const routeTransitionDuration = 1000;
+const gradientRoutes = ['/', '/login'];
 
 export default class CasaApp extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
+    var isGradientRoute = gradientRoutes.indexOf(this.props.location.pathname) > -1;
     return (
       <div className='main'>
         <HeaderContainer />
-        <ReactCSSTransitionReplace
-          component="div"
-          transitionName="route-main"
-          transitionEnterTimeout={reouteTransitionDuration * 2}
-          transitionLeaveTimeout={reouteTransitionDuration}>
-          {React.cloneElement(this.props.children, {
-              key: this.props.location.pathname
-            })}
-       </ReactCSSTransitionReplace>
+        <div className={classNames('main-content', {'gradient-route': isGradientRoute})}>
+          <ReactCSSTransitionReplace
+            component="div"
+            transitionName="route-main"
+            transitionEnterTimeout={routeTransitionDuration * 2}
+            transitionLeaveTimeout={routeTransitionDuration}>
+            {React.cloneElement(this.props.children, {
+                key: this.props.location.pathname
+              })}
+         </ReactCSSTransitionReplace>
+       </div>
       </div>
     );
   }
